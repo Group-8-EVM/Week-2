e62debaf79caf7e1ab9519cd9a89a74bd02d5fec
Daniel <daniel@sohe.ir>
2024-11-04 12:31:41 +0330

    fix: deploy:viem works now

--- scripts/DeployWithViem.ts
+++ scripts/DeployWithViem.ts
@@ -8,26 +8,32 @@ import {
 } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
-import { abi, bytecode } from "@artifacts/contracts/Ballot.sol/Ballot.json";
-import { constants } from "@lib/constants";
+import { abi, bytecode } from "../artifacts/contracts/Ballot.sol/Ballot.json";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
 
 async function main() {
   // Fetch proposals
   const proposals = process.argv.slice(2);
+  const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
+
   if (!proposals || proposals.length < 1)
     throw new Error("scripts -> DeployWithViem -> proposals not provided");
   const publicClient = createPublicClient({
     chain: sepolia,
-    transport: http(constants.integrations.alchemy.sepolia),
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
   });
   const blockNumber = await publicClient.getBlockNumber();
   console.log("scripts -> DeployWithViem -> last block number", blockNumber);
 
   // Create a wallet client
   const deployer = createWalletClient({
-    account: privateKeyToAccount(`0x${constants.account.deployerPrivateKey}`),
+    account,
     chain: sepolia,
-    transport: http(constants.integrations.alchemy.sepolia),
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
   });
   console.log(
     "scripts -> DeployWithViem -> deployer address",
