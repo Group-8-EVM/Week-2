1744b818f4e1eaa6d978fd1c9b6b62455606fb8d
Daniel <daniel@sohe.ir>
2024-11-04 11:45:11 +0330

    fix: get private and api key from env

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,18 @@
 import { task, type HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
-import { constants } from "./lib/constants";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   networks: {
     sepolia: {
       // url: "https://ethereum-sepolia-rpc.publicnode.com",
-      url: constants.integrations.alchemy.sepolia,
-      accounts: [constants.account.deployerPrivateKey],
+      url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
+      accounts: [deployerPrivateKey],
     }
   },
 };
