911f513be234dbb159ad1bd0f94b87bed07ec1af
Daniel <daniel@sohe.ir>
2024-11-04 14:16:46 +0330

    fix: delegrate just checks if already voted ro not

--- scripts/Delegate.ts
+++ scripts/Delegate.ts
@@ -8,8 +8,12 @@ import {
 } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { sepolia } from "viem/chains";
-import { abi } from "@artifacts/contracts/Ballot.sol/Ballot.json";
-import { constants } from "@lib/constants";
+import { abi } from "../artifacts/contracts/Ballot.sol/Ballot.json";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY || "";
 
 async function main() {
   // Fetch parameters
@@ -18,12 +22,10 @@ async function main() {
   const parameters = process.argv.slice(2);
   const targetAddress = parameters[ARG_TARGET_ADDRESS_IDX] as `0x${string}`;
   const contractAddress =
-    (parameters[ARG_CONTRACT_ADDRESS_IDX] as `0x${string}`) ||
-    constants.contracts.ballot.sepolia;
-
-  if (!parameters || parameters.length < 1)
+    (parameters[ARG_CONTRACT_ADDRESS_IDX] as `0x${string}`)
+  if (!parameters || parameters.length < 2)
     throw new Error(
-      "Parameters not provided. You must at least provide the target voter address.",
+      "Parameters not provided. You must provide the target voter address and the contract address.",
     );
 
   if (!targetAddress) throw new Error("Target voter address not provided.");
@@ -47,19 +49,17 @@ async function main() {
   );
   const publicClient = createPublicClient({
     chain: sepolia,
-    transport: http(constants.integrations.alchemy.sepolia),
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
   });
   const blockNumber = await publicClient.getBlockNumber();
   console.log("scripts -> Delegate -> last block number", blockNumber);
 
   // Create a wallet client
-  const deployer = privateKeyToAccount(
-    `0x${constants.account.deployerPrivateKey}`,
-  );
+  const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
   const walletClient = createWalletClient({
-    account: deployer,
+    account,
     chain: sepolia,
-    transport: http(constants.integrations.alchemy.sepolia),
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
   });
   console.log(
     "scripts -> Delegate -> deployer address",
@@ -76,7 +76,7 @@ async function main() {
 
   // Validate that the contract write will execute without errors.
   const { request } = await publicClient.simulateContract({
-    account: deployer,
+    account,
     address: contractAddress,
     abi,
     functionName: "delegate",
